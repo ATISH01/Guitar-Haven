b36884946cd3c3f867f8976405a6d2490c80a4bc
ATISH01 <ab.atish.ab@gmail.com>
2022-03-26 15:03:56 +0600

    price

--- src/components/Guitars/Guitars.js
+++ src/components/Guitars/Guitars.js
@@ -8,7 +8,7 @@ import './Guitars.css'
 
 const Guitars = (props) => {
     const{handleEvent,guitar}=props;
-    const {img,name}=guitar;
+    const {img,name,price}=guitar;
 
     
     return (
@@ -16,9 +16,12 @@ const Guitars = (props) => {
             <div className='image-wrapper'>
             <h4>{name}</h4>
             <img src={img} alt="" />
+            <p>Price:{price}$</p>
             </div>
             <div className="icon">
+                
             <button onClick={()=>handleEvent(guitar)} 
+            
             className='btn'><p>Buy</p><FontAwesomeIcon id='FontAwesomeIcon' icon={faShoppingBasket} /></button>
              
             </div>
